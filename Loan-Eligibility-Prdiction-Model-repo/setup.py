1eb588ed4a56f99adaef363a32d9a37fd528d49d
MD IRTEZA CHOWDHURY <md.irtezachowdhury@gmail.com>
2024-08-04 19:28:42 -0400

    Published

---
+++ Loan-Eligibility-Prdiction-Model-repo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build a model to predict loan eligiblity with an average acuracy of more than 76%',
+    author='Irteza',
+    license='',
+)
